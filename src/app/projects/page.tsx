30a895362e8fe26edf06fc7dafa6bd4f224378d1
asif12018 <asif.request@gmail.com>
2026-04-30 16:22:44 +0600

    improved

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,9 +13,9 @@ const projects = [
     title: 'CineTube',
     description:
       'A cinematic movie discovery and streaming platform. Features real-time search, curated movie rows, community reviews with spoiler warnings, and tiered subscription plans.',
-    tags: ['Next.js 16', 'TypeScript', 'Tailwind v4'],
+    tags: ['Next.js 16', 'TypeScript', 'Prisma'],
     image:
-      'https://lh3.googleusercontent.com/aida-public/AB6AXuDq2WRnJ1al_Ooc_n40X4uptvTu7n-Wz-Hu6w6AFW5ivL5P83rYxTCANH75PAqT0iMyogi4wFR0uV5TJvprpLOaevAoqrgzjPrQvc0Exeb5WgRnPeqs990rem0W16ssENHa7lFd8aD1cri5Ju2j1a5m9Phv9VOBwH-PmPigXZn5TQDbtjo_QUoA78y_dTk-V7lsXYMN0GhlPiFPNNKwefJ7x2NCip_mMt424MiyNdnmdhNvRDU0TcGOUeY3mowdGDXFMiqaN1tMJQ6O',
+      '/cine-tube.jpg',
     alt: 'CineTube App Preview',
     offset: false,
   },
@@ -26,32 +26,32 @@ const projects = [
       'A production-ready food ordering platform connecting customers with providers. Orchestrates complex shopping carts, profile management, and cuisine filtering with smooth GSAP animations.',
     tags: ['React 19', 'better-auth', 'Framer Motion'],
     image:
-      'https://lh3.googleusercontent.com/aida-public/AB6AXuA2va8KJR4MmpPChphS9kY4V4sF5iyywJHtkwUFof24wyJZDdMIbv89AD-oz1sID8n_pfxFRfV1SkpIMqbhqoDvfyV63KEzQKG-zwiK_49b4ywLWc6SYhvrx2cHEUmzI4vNekRbwafc_g7kLjy2hMvoT9-e46nBOOUMJDaGQPTMRa0QBQxLSWQd7J36NQ4XnB_Wj_BgMuvUAAnG6kavLMBFG9bFeaHw-jZc4-za8K8SOTzlVbYgaAyL9hBMvX-o-csFW8yVCSMOX_ii',
+      '/foodhub.jpg',
     alt: 'FoodHub Storefront Preview',
     offset: true,
   },
-  {
-    id: 3,
-    title: 'CineTube Admin System',
-    description:
-      'A dedicated administration interface for content and community management. Provides full CRUD operations for metadata, cast management, and a robust user review moderation system.',
-    tags: ['React Query v5', 'Zod', 'Cloudinary'],
-    image:
-      'https://lh3.googleusercontent.com/aida-public/AB6AXuCSqiVAhq8gs5VWU2DniamXF-6FMgxslbAxlA4y8W253xNUoz9hu0QarQqtSUR5uOMQWMH4Tc2HnUENTDEmm7xrQVGUKcpNsk2Z7UXiGM4l-poHjTWByW-x1d5JtlPW1zlNa6pb3xdPvVaAbfpeYDAGA2JldBz1_pLX1VqzRUW51l20Dsw4WKWr9rVH2qQLbjDVqvsPioFphEW9OlK9vpvVa3FQvcz9GF3jP5la0iFFPlVngtU5DGKy_GOL7qrH7IDjc7taKvwGogRD',
-    alt: 'CineTube Admin Preview',
-    offset: false,
-  },
-  {
-    id: 4,
-    title: 'FoodHub Admin Dashboard',
-    description:
-      'A powerful analytics portal utilizing Next.js parallel routes for role-based access. Features interactive Recharts and feature-rich TanStack data tables for comprehensive order management.',
-    tags: ['TanStack Table', 'Recharts', 'shadcn/ui'],
-    image:
-      'https://lh3.googleusercontent.com/aida-public/AB6AXuDQUYffcU3g-nVcIsFSpCeVnSAKjAV3z36duXFS8Ho_kROBsMBCj6bbamQwCH3EdwvAyr64_nIAOowgcvX3zd_1Vf4zeHzr9YNWZCz87OhzNE0vqLUGjd79WRD43gGPhyAZuO3VhQBbST9FcHwB5gvlMdQNNr9N9ERfb-YGA42ud_7r3qjipYeupP3ngmJPZW_jXLVtNcHKWl6-et-xPJNCQPEpnD9xatxaUNYnzkq97ok46xGBId8CeY9va70XxWx2ofgHZ4788Jna',
-    alt: 'FoodHub Admin Preview',
-    offset: true,
-  },
+  // {
+  //   id: 3,
+  //   title: 'CineTube Admin System',
+  //   description:
+  //     'A dedicated administration interface for content and community management. Provides full CRUD operations for metadata, cast management, and a robust user review moderation system.',
+  //   tags: ['React Query v5', 'Zod', 'Cloudinary'],
+  //   image:
+  //     'https://lh3.googleusercontent.com/aida-public/AB6AXuCSqiVAhq8gs5VWU2DniamXF-6FMgxslbAxlA4y8W253xNUoz9hu0QarQqtSUR5uOMQWMH4Tc2HnUENTDEmm7xrQVGUKcpNsk2Z7UXiGM4l-poHjTWByW-x1d5JtlPW1zlNa6pb3xdPvVaAbfpeYDAGA2JldBz1_pLX1VqzRUW51l20Dsw4WKWr9rVH2qQLbjDVqvsPioFphEW9OlK9vpvVa3FQvcz9GF3jP5la0iFFPlVngtU5DGKy_GOL7qrH7IDjc7taKvwGogRD',
+  //   alt: 'CineTube Admin Preview',
+  //   offset: false,
+  // },
+  // {
+  //   id: 4,
+  //   title: 'FoodHub Admin Dashboard',
+  //   description:
+  //     'A powerful analytics portal utilizing Next.js parallel routes for role-based access. Features interactive Recharts and feature-rich TanStack data tables for comprehensive order management.',
+  //   tags: ['TanStack Table', 'Recharts', 'shadcn/ui'],
+  //   image:
+  //     'https://lh3.googleusercontent.com/aida-public/AB6AXuDQUYffcU3g-nVcIsFSpCeVnSAKjAV3z36duXFS8Ho_kROBsMBCj6bbamQwCH3EdwvAyr64_nIAOowgcvX3zd_1Vf4zeHzr9YNWZCz87OhzNE0vqLUGjd79WRD43gGPhyAZuO3VhQBbST9FcHwB5gvlMdQNNr9N9ERfb-YGA42ud_7r3qjipYeupP3ngmJPZW_jXLVtNcHKWl6-et-xPJNCQPEpnD9xatxaUNYnzkq97ok46xGBId8CeY9va70XxWx2ofgHZ4788Jna',
+  //   alt: 'FoodHub Admin Preview',
+  //   offset: true,
+  // },
   {
     id: 5,
     title: 'Edura',
@@ -59,7 +59,7 @@ const projects = [
       'An online educational platform where users can enroll in and complete courses. Features a responsive video player, secure payments, and role-based access for students, teachers, and admins.',
     tags: ['React', 'Node.js', 'Express', 'Firebase'],
     image:
-      'https://lh3.googleusercontent.com/aida-public/AB6AXuCSqiVAhq8gs5VWU2DniamXF-6FMgxslbAxlA4y8W253xNUoz9hu0QarQqtSUR5uOMQWMH4Tc2HnUENTDEmm7xrQVGUKcpNsk2Z7UXiGM4l-poHjTWByW-x1d5JtlPW1zlNa6pb3xdPvVaAbfpeYDAGA2JldBz1_pLX1VqzRUW51l20Dsw4WKWr9rVH2qQLbjDVqvsPioFphEW9OlK9vpvVa3FQvcz9GF3jP5la0iFFPlVngtU5DGKy_GOL7qrH7IDjc7taKvwGogRD',
+      '/edura.jpg',
     alt: 'Edura Platform Preview',
     offset: false,
   },
